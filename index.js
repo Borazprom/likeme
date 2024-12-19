160ddd0a3627408be3ccba83c6087ae8cac11137
Borazprom <felipe.borbaran@gmail.com>
2024-12-18 23:28:39 -0300

    uniendo con el fontrend

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { agregarPost, obtenerPosts } = require("./posts");
 const express = require("express");
 const app = express();
-app.use(express.json());
+const cors = require("cors");
+app.use(express.json(), cors());
+// const port = 3000;
 
+// app.listen(port, () => console.log(`SERVIDOR ENCENDIDO" ${port}`));
 app.listen(3000, console.log("SERVIDOR ENCENDIDO"));
 
 app.get("/posts", async (req, res) => {
